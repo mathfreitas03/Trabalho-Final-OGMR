c4464235b333cec35aff1098c476275baa95addf
mathfreitas03 <matheus.lopesfreitas@hotmail.com>
2025-12-01 00:06:24 -0300

    Fix da Main

--- network-manager/src/main/java/com/org/Main.java
+++ network-manager/src/main/java/com/org/Main.java
@@ -1,61 +1,30 @@
 package com.org;
 
-
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-import org.snmp4j.CommunityTarget;
-import org.snmp4j.PDU;
-// import org.snmp4j.Target;
-import org.snmp4j.Snmp;
-import org.snmp4j.TransportMapping;
-import org.snmp4j.event.ResponseEvent;
-import org.snmp4j.mp.SnmpConstants;
-// import org.snmp4j.smi.GenericAddress;
-import org.snmp4j.smi.OID;
-import org.snmp4j.smi.OctetString;
-import org.snmp4j.smi.UdpAddress;
-import org.snmp4j.smi.VariableBinding;
-import org.snmp4j.transport.DefaultUdpTransportMapping;
-// import org.snmp4j.transport.UdpTransportMapping;
-
 import com.org.db.PostgresPool;
+import com.org.snmp.SnmpTrapListener;
 import com.org.web.HttpServer;
 
 public class Main {
+
     public static void main(String[] args) {
         try {
+            // 1 Inicializa pool do banco
+            PostgresPool.init("localhost", 5432, "ogmr", "postgres", "udesc");
+            System.out.println("Pool do banco inicializada.");
+
+            // 2️ Inicializa o servidor HTTP em uma thread separada
             HttpServer server = new HttpServer(8080);
-            server.start();
+            Thread serverThread = new Thread(server::start);
+            serverThread.start();
+            System.out.println("Servidor HTTP iniciado na porta 8080.");
 
-            TransportMapping transport = new DefaultUdpTransportMapping();
-            Snmp snmp = new Snmp(transport);
-            transport.listen();
+            // 3 Inicializa o listener SNMP (porta 162)
+            SnmpTrapListener trapListener = new SnmpTrapListener(162);
+            System.out.println("Listener SNMP ativo na porta 162.");
 
-            CommunityTarget target = new CommunityTarget();
-            // TODO: Usar comunidade "private" para sets
-            target.setCommunity(new OctetString("public"));
-            // TODO: Configurar endereço para 10.90.90.90 - Não esquecer de setar o IP do servidor e do cliente manualmente
-            target.setAddress(new UdpAddress("127.0.0.1/161"));
-            // TODO: Versão do SWITCH é 1
-            target.setVersion(SnmpConstants.version2c);
-            target.setRetries(2);
-            target.setTimeout(1500);
+            // O main thread continua vivo para manter listener e servidor ativos
+            serverThread.join();
 
-            PDU pdu = new PDU();
-            pdu.add(new VariableBinding(new OID("1.3.6.1.2.1.1.3.0")));
-            pdu.setType(PDU.GET);
-
-            ResponseEvent response = snmp.send(pdu, target);
-
-            if(response != null){
-                System.out.println(response.getResponse().get(0).getVariable().toString());
-            } else {
-                System.out.println("Erro ao obter response");
-            }
-            snmp.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
---
+++ network-manager/src/main/java/com/org/Main2.java
@@ -0,0 +1,63 @@
+package com.org;
+
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.snmp4j.CommunityTarget;
+import org.snmp4j.PDU;
+// import org.snmp4j.Target;
+import org.snmp4j.Snmp;
+import org.snmp4j.TransportMapping;
+import org.snmp4j.event.ResponseEvent;
+import org.snmp4j.mp.SnmpConstants;
+// import org.snmp4j.smi.GenericAddress;
+import org.snmp4j.smi.OID;
+import org.snmp4j.smi.OctetString;
+import org.snmp4j.smi.UdpAddress;
+import org.snmp4j.smi.VariableBinding;
+import org.snmp4j.transport.DefaultUdpTransportMapping;
+// import org.snmp4j.transport.UdpTransportMapping;
+
+import com.org.db.PostgresPool;
+import com.org.web.HttpServer;
+
+public class Main2 {
+    public static void main(String[] args) {
+        try {
+            HttpServer server = new HttpServer(8080);
+            server.start();
+
+            TransportMapping transport = new DefaultUdpTransportMapping();
+            Snmp snmp = new Snmp(transport);
+            transport.listen();
+
+            CommunityTarget target = new CommunityTarget();
+            // TODO: Usar comunidade "private" para sets
+            target.setCommunity(new OctetString("public"));
+            // TODO: Configurar endereço para 10.90.90.90 - Não esquecer de setar o IP do servidor e do cliente manualmente
+            target.setAddress(new UdpAddress("127.0.0.1/161"));
+            // TODO: Versão do SWITCH é 1
+            target.setVersion(SnmpConstants.version2c);
+            target.setRetries(2);
+            target.setTimeout(1500);
+
+            PDU pdu = new PDU();
+            pdu.add(new VariableBinding(new OID("1.3.6.1.2.1.1.3.0")));
+            pdu.setType(PDU.GET);
+
+            ResponseEvent response = snmp.send(pdu, target);
+
+            if(response != null){
+                System.out.println(response.getResponse().get(0).getVariable().toString());
+            } else {
+                System.out.println("Erro ao obter response");
+            }
+            snmp.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
